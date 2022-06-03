8f02dc975b5a1f5d001528c721d5449e9a7de11c
Timi-T <timiopebode@gmail.com>
2022-06-03 20:39:48 +0100

    data filtering and url fix

--- ansmake_app/urls.py
+++ ansmake_app/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('new/', views.createTask, name='create-task'),
-    path('all/', views.viewTasks, name='view-task'),
+    path('new', views.createTask, name='create-task'),
+    path('all', views.viewTasks, name='view-task'),
 ]
--- ansmake_app/views.py
+++ ansmake_app/views.py
@@ -3,28 +3,40 @@ Module containing all route functions
 """
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from .models import Task
 import json
 
 
+@csrf_exempt
 def createTask(request):
     """Function to create a task"""
 
-    #info = {"name": "ope", "email": "ope@gmail.com", "description": "first task"}
-    info = request.body
-    info = json.loads(info)
-    new_task = Task(**info)
-    new_task.save()
+    data = request.body
+    data = json.loads(data)
+    info = {}
+    name = info['name'] = data.get('name')
+    email = info['email'] = data.get('email')
+    description = info['description'] = data.get('description')
+    if not description:
+        info['description'] = ''
+    if name and email:
+        new_task = Task(**info)
+        new_task.save()
     return HttpResponse()
 
 def viewTasks(request):
     """Function to view all tasks"""
 
-    task_dict = []
+    task_list = []
     all_tasks = Task.objects.all()
     for task in all_tasks:
-        task_dict.append(task.to_dict())
+        """Converting all the objects in the list to dictionaries"""
+        task_list.append(task.to_dict())
+        """
+        to_dict is a method created to convert a 'Task' object to a dictionary
+        """
     
-    res = {'data': task_dict}
+    res = {'data': task_list}
     res = JsonResponse(res)
     return HttpResponse(res)
